2cc91de784457c3a05aa561ab447e3a0b6ef4e2c
Colin Joss <colindjoss@gmail.com>
2021-02-21 13:03:43 -0800

    Pulling recently played spotify songs via spotipy module

--- main.py
+++ main.py
@@ -2,60 +2,35 @@
 # Last date updated: 2-17-2021
 # Description:
 
-
-# import requests
-# response = requests.get("https://api.spotify.com")
-# spotify = response.json()
-# print(spotify)
-
-import sqlalchemy
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import pandas
-from sqlalchemy.orm import sessionmaker
-import requests
-import json
-import datetime
-import sqlite3
-import base64
 
-DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID = "12129815625"
-TOKEN = "BQBK0Y7Lo5Ge-tJ1Sz75ieTxVFkNhXEI8T5OGP5uKqsw23KPIUa9mC70vNhc3jQrlbADx08CWJPCgN2Tvyc139xR5gb9ENALlqo7XHLBzeE_cI8PXvBpylZZHnqG2a-NZfAZ7zL1m5b9agh1uUrLMw"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id="7406bc4c83534c57bdc0cf3e8b6991d5",
+    client_secret="6d5fa9a24d444ef89793c953aa88d87a",
+    redirect_uri="https://www.google.com/",
+    scope="user-read-recently-played"))
 
-if __name__ == "__main__":
+song_names = []
+artist_names = []
+played_at_list = []
+timestamps = []
 
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {TOKEN}"
-    }
+results = sp.current_user_recently_played(limit=50)
+for song in results["items"]:
+    song_names.append(song["track"]["name"])
+    artist_names.append(song["track"]["album"]["artists"][0]["name"])
+    played_at_list.append(song["played_at"])
+    timestamps.append(song["played_at"][0:10])
 
-    today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
-    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
-    now_unix_timestamp = int(today.timestamp()) * 1000
+song_dict = {
+    "song_name": song_names,
+    "artist_name": artist_names,
+    "played_at": played_at_list,
+    "timestamps": timestamps
+}
 
-    response = requests.get(f"https://api.spotify.com/v1/me/player/recently-played?limit=50&before={now_unix_timestamp}",
-                            headers=headers).json()
-    print(response)
+song_df = pandas.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamps"])
 
-    song_names = []
-    artist_names = []
-    played_at_list = []
-    timestamps = []
-
-    for song in response["items"]:
-        song_names.append(song["track"]["name"])
-        artist_names.append(song["track"]["album"]["artists"][0]["name"])
-        played_at_list.append(song["played_at"])
-        timestamps.append(song["played_at"][0:10])
-
-    song_dict = {
-        "song_name": song_names,
-        "artist_name": artist_names,
-        "played_at": played_at_list,
-        "timestamps": timestamps
-    }
-
-    song_df = pandas.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamps"])
-
-    print(song_df)
+print(song_df)
